e95adbad05525d001c8bfcce2d6171fc1a1068d2
xuxudong <xuxudong@cvte.com>
2020-04-03 11:06:54 +0800

    first commit

---
+++ algorithms-array/src/main/java/com/starry/pratice/ArrayTest.java
@@ -0,0 +1,24 @@
+package com.starry.pratice;
+
+/**
+ * @Description
+ * @Auther: xuxudong@cvte.com
+ * @Date: 2020/4/3 10:07
+ */
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        Array<Integer> integerArray = new Array<>(2);
+
+        integerArray.addLast(1);
+        integerArray.addLast(2);
+        System.out.println(integerArray);
+        integerArray.addLast(3);
+        System.out.println(integerArray);
+
+        integerArray.removeLast();
+        System.out.println(integerArray);
+        integerArray.removeFirst();
+        System.out.println(integerArray);
+    }
+}
