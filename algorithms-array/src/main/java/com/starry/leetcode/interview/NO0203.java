08b057e047e8a575a8d973d8877f434d4297a9e4
xuxudong <xuxudong@cvte.com>
2020-04-20 10:51:39 +0800

    增加对部分力扣的解答

---
+++ algorithms-array/src/main/java/com/starry/leetcode/interview/NO0203.java
@@ -0,0 +1,34 @@
+package com.starry.leetcode.interview;
+
+/**
+ * @Description 实现一种算法，删除单向链表中间的某个节点（除了第一个和最后一个节点，不一定是中间节点），假定你只能访问该节点。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：单向链表a->b->c->d->e->f中的节点c
+ * 结果：不返回任何数据，但该链表变为a->b->d->e->f
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/delete-middle-node-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Auther: https://github.com/starry-eyed-art
+ * @Date: 2020/4/20 10:18 上午
+ */
+public class NO0203 {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
